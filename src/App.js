2fec98b3f6ee5f23b672d9f0498b721ff11aec83
mayank.kushwah <mayank.kushwah@eagleview.com>
2024-10-19 00:04:02 +0530

    Add Mapbox GL integration and environment configuration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOXGL_ACCESS_TOKEN;
 
-export default App;
+const MapComponent = () => {
+  const mapContainer = useRef(null);
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [102.0, 0.5],
+      zoom: 8,
+    });
+
+    map.on('load', () => {
+      map.addSource('parcels', {
+        type: 'geojson',
+        data: 'https://gist.githubusercontent.com/wavded/1200773/raw/e122cf709898c09758aecfef349964a8d73a83f3/sample.json',
+      });
+
+      map.addLayer({
+        id: 'parcel-layer',
+        type: 'fill',
+        source: 'parcels',
+        paint: {
+          'fill-color': '#888888',
+          'fill-opacity': 0.4,
+        },
+      });
+
+      map.addLayer({
+        id: 'parcel-outline',
+        type: 'line',
+        source: 'parcels',
+        paint: {
+          'line-color': '#000000',
+          'line-width': 5,
+        },
+      });
+    });
+
+    return () => map.remove();
+  }, []);
+
+  return <div ref={mapContainer} style={{ width: '100%', height: '100vh' }} />;
+};
+
+export default MapComponent;
